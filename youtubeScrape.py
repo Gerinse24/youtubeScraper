c0a40f18a98ee8e7a5948da5e6630f5c283ead6d
G.A.Shaw <geronimo.shaw@gmail.com>
2019-11-07 16:52:01 +0800

    v0.02

--- youtubeScrape.py
+++ youtubeScrape.py
@@ -4,6 +4,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+import sys
 
 options = Options()
 options.headless = True
@@ -26,4 +27,8 @@ url.close()
 vfin = v[0::2]   # Creates new list from v starting at index 0 and stepping over twice
 
 for i in range(len(t)):
-    print(t[i] + ' ' + vfin[i])
+    print('Video Title: %s - Views: %s' % (t[i], vfin[i]))
+
+ex = input('When you are done type exit.\n')
+if ex == 'exit':
+    sys.exit()
